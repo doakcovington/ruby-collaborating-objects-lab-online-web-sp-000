913d58cb1857a0a2eb76697ea35bc003a8595e92
Doak Covington <doakcovington@protonmail.com>
2020-02-14 05:12:50 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,45 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name, :songs
+
+  @@all = []
+binding.pry
+  def initialize(name)
+    @name = name
+    @songs = []
+    save
+  end
+
+  def save
+    @@all << self
+    self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    self.songs << song
+  end
+
+  def self.find_or_create_by_name(name)
+    artist = @@all.find {|artist| artist.name == name}
+    if artist
+      artist
+    else
+      new_artist = self.new(name)
+      new_artist.save
+    end
+  end
+
+  def print_songs
+    @songs.each{|song| puts song.name}
+  end
+
+end
