ad78a068341a4e25bb2893d811ba909c9decc5b6
Doak Covington <doakcovington@protonmail.com>
2020-02-14 05:18:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ class Artist
   attr_accessor :name, :songs
 
   @@all = []
+
   def initialize(name)
     @name = name
     @songs = []
@@ -24,7 +25,7 @@ class Artist
   end
 
   def add_song(song)
-    self.songs << song
+    song.artist = self
   end
 
   def self.find_or_create_by_name(name)
